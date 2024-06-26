787d43f6d53a3fd07325ef708b72a4a9be186f4f
Gideon Atauba <ataubagideon@gmail.com>
2024-06-26 22:07:21 +0100

    updated scrolling effect

--- src/util/animation.js
+++ src/util/animation.js
@@ -4,6 +4,8 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger'
 gsap.registerPlugin(ScrollTrigger)
 
 export function homeAnimation() {
+  console.log('homeAnimation initialized')
+
   gsap.fromTo(
     '.expand',
     { y: 100, opacity: 0 },
@@ -21,7 +23,7 @@ export function homeAnimation() {
   const tl = gsap.timeline({
     scrollTrigger: {
       trigger: '.home-info',
-      start: 'top 80%',
+      start: 'top 90%', 
       toggleActions: 'play none none none',
     },
   })
@@ -30,8 +32,7 @@ export function homeAnimation() {
     '.home-info article:first-child',
     { y: -100, opacity: 0 },
     { y: 0, opacity: 1, ease: 'expo.out', duration: 2 }
-  )
-  tl.fromTo(
+  ).fromTo(
     '.home-info article:last-child',
     { y: 100, opacity: 0 },
     { y: 0, opacity: 1, ease: 'expo.out', duration: 2 }
@@ -39,6 +40,7 @@ export function homeAnimation() {
 }
 
 export function aboutAnimation() {
+  console.log('aboutAnimation initialized')
 
   const tl = gsap.timeline({
     scrollTrigger: {
@@ -57,25 +59,24 @@ export function aboutAnimation() {
       duration: 2.5,
     }
   )
-
-  tl.fromTo(
-    '.slides.slide-1',
-    { opacity: 0 },
-    { opacity: 1, duration: 1 },
-    '-=2'
-  )
-  tl.fromTo(
-    '.slides.slide-2',
-    { opacity: 0 },
-    { opacity: 1, duration: 1 },
-    '-=1.5'
-  )
-  tl.fromTo(
-    '.slides.slide-3',
-    { opacity: 0 },
-    { opacity: 1, duration: 1 },
-    '-=1'
-  )
+    .fromTo(
+      '.slides.slide-1',
+      { opacity: 0 },
+      { opacity: 1, duration: 1 },
+      '-=2'
+    )
+    .fromTo(
+      '.slides.slide-2',
+      { opacity: 0 },
+      { opacity: 1, duration: 1 },
+      '-=1.5'
+    )
+    .fromTo(
+      '.slides.slide-3',
+      { opacity: 0 },
+      { opacity: 1, duration: 1 },
+      '-=1'
+    )
 
   // GUIDES
   gsap.fromTo(
@@ -110,7 +111,7 @@ export function aboutAnimation() {
     }
   )
 
-  //ROLES
+  // ROLES
   gsap.fromTo(
     '.roles',
     { y: 100, opacity: 0 },
@@ -148,6 +149,8 @@ export function aboutAnimation() {
 }
 
 export function pricingAnimation() {
+  console.log('pricingAnimation initialized')
+
   gsap.fromTo(
     '.pricing-container',
     { y: 100, opacity: 0 },
@@ -177,72 +180,69 @@ export function pricingAnimation() {
       { y: 100, opacity: 0 },
       { y: 0, opacity: 1, duration: 2, ease: 'power3.out' }
     )
-
-    tl.fromTo(
-      '.call-info h1',
-      { y: 100, opacity: 0 },
-      { y: 0, opacity: 1, duration: 2.5, ease: 'expo.out' }
-    )
-
-    tl.fromTo(
-      '.call-list li',
-      { y: 50, opacity: 0 },
-      { y: 0, opacity: 1, duration: 1, ease: 'power3.out', stagger: 0.2 }
-    )
-
-    tl.fromTo(
-      ['.call-links'],
-      { y: 50, opacity: 0 },
-      { y: 0, opacity: 1, duration: 0.5, ease: 'power3.out' },
-      '-=0.5'
-    )
+      .fromTo(
+        '.call-info h1',
+        { y: 100, opacity: 0 },
+        { y: 0, opacity: 1, duration: 2.5, ease: 'expo.out' }
+      )
+      .fromTo(
+        '.call-list li',
+        { y: 50, opacity: 0 },
+        { y: 0, opacity: 1, duration: 1, ease: 'expo.out', stagger: 0.2 },
+        '-=2'
+      )
+      .fromTo(
+        '.call-links',
+        { y: 50, opacity: 0 },
+        { y: 0, opacity: 1, duration: 0.5, ease: 'power3.out' },
+        '-=1'
+      )
   } else {
     tl.fromTo(
       '.call-info h1',
       { y: 100, opacity: 0 },
       { y: 0, opacity: 1, duration: 1, ease: 'power3.out' }
     )
-
-    tl.fromTo(
-      '.call-list li',
-      { y: 50, opacity: 0 },
-      { y: 0, opacity: 1, duration: 1, ease: 'power3.out', stagger: 0.2 }
-    )
-
-    tl.fromTo(
-      ['.call-links'],
-      { y: 50, opacity: 0 },
-      { y: 0, opacity: 1, duration: 1, ease: 'power3.out' },
-      '-=0.5'
-    )
-
-    tl.fromTo(
-      '.callImg',
-      { x: 100, opacity: 0 },
-      { x: 0, opacity: 1, duration: 2, ease: 'power3.out' },
-      '-=1.1'
-    )
+      .fromTo(
+        '.call-list li',
+        { y: 50, opacity: 0 },
+        { y: 0, opacity: 1, duration: 1, ease: 'power3.out', stagger: 0.2 }
+      )
+      .fromTo(
+        '.call-links',
+        { y: 50, opacity: 0 },
+        { y: 0, opacity: 1, duration: 1, ease: 'power3.out' },
+        '-=0.5'
+      )
+      .fromTo(
+        '.callImg',
+        { x: 100, opacity: 0 },
+        { x: 0, opacity: 1, duration: 2, ease: 'power3.out' },
+        '-=1.1'
+      )
   }
 
   // hba
-
   gsap.fromTo(
     '.client-text',
-    {
-      y: 100,
-      opacity: 0,
-   
-    },
+    { y: 100, opacity: 0 },
     {
       y: 0,
       opacity: 1,
       ease: 'expo.out',
       duration: 2.5,
+      scrollTrigger: {
+        trigger: '.client-text',
+        start: 'top bottom',
+        toggleActions: 'play none none none',
+      },
     }
   )
 }
 
 export function faqAnimation() {
+  console.log('faqAnimation initialized')
+
   gsap.fromTo(
     '.faq-header',
     { y: 100, opacity: 0 },
@@ -281,25 +281,10 @@ export function faqAnimation() {
 }
 
 export function footerAnimation() {
-  gsap.fromTo(
-    '.techiesImg',
-    { y: 100, opacity: 0 },
-    {
-      y: 0,
-      opacity: 1,
-      ease: 'expo.out',
-      duration: 2.5,
-      scrollTrigger: {
-        trigger: '.footer-info',
-        start: 'top bottom',
-        toggleActions: 'play none none none',
-        once: true,
-      },
-    }
-  )
+  console.log('footerAnimation initialized')
 
   gsap.fromTo(
-    '.details',
+    '.techiesImg',
     { y: 100, opacity: 0 },
     {
       y: 0,
