369905d5039ae3264d60ddef7c816295a1b50650
Gideon Atauba <ataubagideon@gmail.com>
2024-06-26 21:59:12 +0100

    added smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import { createBrowserRouter,RouterProvider } from "react-router-dom"
-import RootLayout from "./pages/Root"
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import RootLayout from './pages/Root'
+import Lenis from '@studio-freight/lenis'
+import { useEffect } from 'react'
 
-const router = createBrowserRouter([
-  {path: '/', element: <RootLayout /> }
-])
+
+
+const router = createBrowserRouter([{ path: '/', element: <RootLayout /> }])
 
 function App() {
-  return (
-    <RouterProvider router={router}>
-    </RouterProvider>
-  )
+
+  useEffect(() => {
+    const lenis = new Lenis()
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+    requestAnimationFrame(raf)
+  }, [])
+  return <RouterProvider router={router}></RouterProvider>
 }
 
 export default App
